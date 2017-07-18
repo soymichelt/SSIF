c6c762559514b2d2af71df0ec3d00835278eb9b5
Michael Traña <mtraatabladaa94@gmail.com>
2017-07-18 17:51:41 -0600

    Avances Dashboard III

--- Aplicación/appFacturacion/appFacturacion/Views/Reports/frmDashboard.Designer.vb
+++ Aplicación/appFacturacion/appFacturacion/Views/Reports/frmDashboard.Designer.vb
@@ -19,26 +19,26 @@ Partial Class frmDashboard
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Dim XyDiagram1 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
-        Dim Series1 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
-        Dim LineSeriesView1 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
-        Dim Series2 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
-        Dim LineSeriesView2 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
-        Dim Series3 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
-        Dim LineSeriesView3 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
-        Dim ChartTitle1 As DevExpress.XtraCharts.ChartTitle = New DevExpress.XtraCharts.ChartTitle()
+        Dim XyDiagram23 As DevExpress.XtraCharts.XYDiagram = New DevExpress.XtraCharts.XYDiagram()
+        Dim Series67 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+        Dim LineSeriesView67 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
+        Dim Series68 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+        Dim LineSeriesView68 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
+        Dim Series69 As DevExpress.XtraCharts.Series = New DevExpress.XtraCharts.Series()
+        Dim LineSeriesView69 As DevExpress.XtraCharts.LineSeriesView = New DevExpress.XtraCharts.LineSeriesView()
+        Dim ChartTitle23 As DevExpress.XtraCharts.ChartTitle = New DevExpress.XtraCharts.ChartTitle()
         Me.chartVentasMensuales = New DevExpress.XtraCharts.ChartControl()
         Me.GroupControl1 = New DevExpress.XtraEditors.GroupControl()
         Me.PanelControl1 = New DevExpress.XtraEditors.PanelControl()
         Me.SplitterControl1 = New DevExpress.XtraEditors.SplitterControl()
         CType(Me.chartVentasMensuales, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(XyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Series1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(LineSeriesView1, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Series2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(LineSeriesView2, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Series3, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(LineSeriesView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(XyDiagram23, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Series67, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(LineSeriesView67, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Series68, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(LineSeriesView68, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Series69, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(LineSeriesView69, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.PanelControl1.SuspendLayout()
@@ -48,9 +48,9 @@ Partial Class frmDashboard
         '
         Me.chartVentasMensuales.BorderOptions.Visibility = DevExpress.Utils.DefaultBoolean.[True]
         Me.chartVentasMensuales.DataBindings = Nothing
-        XyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
-        XyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
-        Me.chartVentasMensuales.Diagram = XyDiagram1
+        XyDiagram23.AxisX.VisibleInPanesSerializable = "-1"
+        XyDiagram23.AxisY.VisibleInPanesSerializable = "-1"
+        Me.chartVentasMensuales.Diagram = XyDiagram23
         Me.chartVentasMensuales.Dock = System.Windows.Forms.DockStyle.Fill
         Me.chartVentasMensuales.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Center
         Me.chartVentasMensuales.Legend.AlignmentVertical = DevExpress.XtraCharts.LegendAlignmentVertical.BottomOutside
@@ -63,36 +63,45 @@ Partial Class frmDashboard
         Me.chartVentasMensuales.Location = New System.Drawing.Point(15, 95)
         Me.chartVentasMensuales.Margin = New System.Windows.Forms.Padding(3, 6, 3, 3)
         Me.chartVentasMensuales.Name = "chartVentasMensuales"
-        Series1.Name = "Totales del Año"
-        LineSeriesView1.Color = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(77, Byte), Integer))
-        LineSeriesView1.LineMarkerOptions.Color = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(220, Byte), Integer), CType(CType(219, Byte), Integer))
-        LineSeriesView1.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
-        Series1.View = LineSeriesView1
-        Series2.Name = "Hombres"
-        LineSeriesView2.Color = System.Drawing.Color.FromArgb(CType(CType(155, Byte), Integer), CType(CType(187, Byte), Integer), CType(CType(89, Byte), Integer))
-        LineSeriesView2.LineMarkerOptions.Color = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(221, Byte), Integer))
-        LineSeriesView2.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
-        Series2.View = LineSeriesView2
-        Series3.Name = "Empresa"
-        LineSeriesView3.Color = System.Drawing.Color.FromArgb(CType(CType(79, Byte), Integer), CType(CType(129, Byte), Integer), CType(CType(189, Byte), Integer))
-        LineSeriesView3.LineMarkerOptions.Color = System.Drawing.Color.FromArgb(CType(CType(198, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(240, Byte), Integer))
-        LineSeriesView3.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
-        Series3.View = LineSeriesView3
-        Me.chartVentasMensuales.SeriesSerializable = New DevExpress.XtraCharts.Series() {Series1, Series2, Series3}
+        Series67.Name = "Totales del Año"
+        LineSeriesView67.Color = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(80, Byte), Integer), CType(CType(77, Byte), Integer))
+        LineSeriesView67.LineMarkerOptions.Color = System.Drawing.Color.FromArgb(CType(CType(242, Byte), Integer), CType(CType(220, Byte), Integer), CType(CType(219, Byte), Integer))
+        LineSeriesView67.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
+        Series67.View = LineSeriesView67
+        Series68.Name = "Hombres"
+        LineSeriesView68.Color = System.Drawing.Color.FromArgb(CType(CType(155, Byte), Integer), CType(CType(187, Byte), Integer), CType(CType(89, Byte), Integer))
+        LineSeriesView68.LineMarkerOptions.Color = System.Drawing.Color.FromArgb(CType(CType(235, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(221, Byte), Integer))
+        LineSeriesView68.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
+        Series68.View = LineSeriesView68
+        Series69.Name = "Empresa"
+        LineSeriesView69.Color = System.Drawing.Color.FromArgb(CType(CType(79, Byte), Integer), CType(CType(129, Byte), Integer), CType(CType(189, Byte), Integer))
+        LineSeriesView69.LineMarkerOptions.Color = System.Drawing.Color.FromArgb(CType(CType(198, Byte), Integer), CType(CType(217, Byte), Integer), CType(CType(240, Byte), Integer))
+        LineSeriesView69.MarkerVisibility = DevExpress.Utils.DefaultBoolean.[True]
+        Series69.View = LineSeriesView69
+        Me.chartVentasMensuales.SeriesSerializable = New DevExpress.XtraCharts.Series() {Series67, Series68, Series69}
         Me.chartVentasMensuales.Size = New System.Drawing.Size(545, 530)
         Me.chartVentasMensuales.TabIndex = 0
-        ChartTitle1.Text = "Ventas Mensuales"
-        Me.chartVentasMensuales.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() {ChartTitle1})
+        ChartTitle23.Text = "Ventas Mensuales"
+        Me.chartVentasMensuales.Titles.AddRange(New DevExpress.XtraCharts.ChartTitle() {ChartTitle23})
         '
         'GroupControl1
         '
+        Me.GroupControl1.Appearance.BackColor = System.Drawing.Color.White
+        Me.GroupControl1.Appearance.BackColor2 = System.Drawing.Color.White
+        Me.GroupControl1.Appearance.BorderColor = System.Drawing.Color.White
+        Me.GroupControl1.Appearance.Options.UseBackColor = True
+        Me.GroupControl1.Appearance.Options.UseBorderColor = True
+        Me.GroupControl1.AppearanceCaption.Font = New System.Drawing.Font("Arial", 9.0!, System.Drawing.FontStyle.Bold)
+        Me.GroupControl1.AppearanceCaption.ForeColor = System.Drawing.Color.FromArgb(CType(CType(37, Byte), Integer), CType(CType(48, Byte), Integer), CType(CType(57, Byte), Integer))
+        Me.GroupControl1.AppearanceCaption.Options.UseFont = True
+        Me.GroupControl1.AppearanceCaption.Options.UseForeColor = True
         Me.GroupControl1.Dock = System.Windows.Forms.DockStyle.Top
         Me.GroupControl1.Location = New System.Drawing.Point(15, 18)
         Me.GroupControl1.Name = "GroupControl1"
         Me.GroupControl1.Padding = New System.Windows.Forms.Padding(0, 0, 0, 3)
         Me.GroupControl1.Size = New System.Drawing.Size(545, 77)
         Me.GroupControl1.TabIndex = 2
-        Me.GroupControl1.Text = "Ventas Mensuales : Filtros"
+        Me.GroupControl1.Text = "Ventas Mensuales (Filtros del gráfico)"
         '
         'PanelControl1
         '
@@ -116,6 +125,8 @@ Partial Class frmDashboard
         '
         'frmDashboard
         '
+        Me.Appearance.BackColor = System.Drawing.Color.FromArgb(CType(CType(241, Byte), Integer), CType(CType(241, Byte), Integer), CType(CType(241, Byte), Integer))
+        Me.Appearance.Options.UseBackColor = True
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(859, 643)
@@ -124,13 +135,13 @@ Partial Class frmDashboard
         Me.Name = "frmDashboard"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Dashboard"
-        CType(XyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(LineSeriesView1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Series1, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(LineSeriesView2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Series2, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(LineSeriesView3, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Series3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(XyDiagram23, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(LineSeriesView67, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Series67, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(LineSeriesView68, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Series68, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(LineSeriesView69, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Series69, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.chartVentasMensuales, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.GroupControl1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.PanelControl1, System.ComponentModel.ISupportInitialize).EndInit()
