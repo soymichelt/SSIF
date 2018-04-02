33882faa0d689d01f097f651117fc60194c19ff0
Michael Traña <mtraatabladaa94@gmail.com>
2018-04-02 17:31:39 -0600

    Actualización de Entity Framework

---
+++ App/appFacturacion/Sadara.DataLayer/TransactionToDb/Transaction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Sadara.DataLayer.Transaction;
+using Sadara.Models.V1.Database;
+
+namespace Sadara.DataLayer.TransactionToDb
+{
+
+    class Transaction
+    {
+
+        private CodeFirst db;
+
+        public CodeFirst Db { get; }
+
+        private void closeTransaction()
+        {
+
+            this.db.Dispose();
+
+            this.db = null;
+
+        }
+
+        private Boolean isDbStarted()
+        {
+
+            return this.db != null;
+
+        }
+
+        private void beginTransaction()
+        {
+
+            if(this.isDbStarted())
+                this.db = new CodeFirst();
+
+        }
+
+        private int commit()
+        {
+
+            return this.db.SaveChanges();
+
+        }
+
+        private async Task<int> commitAsync()
+        {
+
+            return await this.db.SaveChangesAsync();
+
+        }
+
+    }
+
+}
