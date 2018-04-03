60d3cabf4d9b045af1ad4f1966d2bd48e96fd567
Michael Traña <mtraatabladaa94@gmail.com>
2018-04-03 15:10:15 -0600

    Añadiendo Formulario de Backup al Repositorio
    Configuración de Carpeta donde se encuentra el formulario 'frmBackup.vb' para evitar que no se almacene en el repositorio el formulario de Backup

---
+++ App/appFacturacion/appFacturacion/Views/Security/frmBackup.Designer.vb
@@ -0,0 +1,169 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmBackup
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmBackup))
+        Me.bkEstilo = New Klik.Windows.Forms.v1.Common.KFormManager(Me.components)
+        Me.ElGroupBox1 = New Klik.Windows.Forms.v1.EntryLib.ELGroupBox()
+        Me.btExamine = New DevComponents.DotNetBar.ButtonX()
+        Me.btBackup = New Klik.Windows.Forms.v1.EntryLib.ELButton()
+        Me.txtPath = New DevComponents.DotNetBar.Controls.TextBoxX()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.fbdExamine = New System.Windows.Forms.FolderBrowserDialog()
+        CType(Me.bkEstilo, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.ElGroupBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.ElGroupBox1.SuspendLayout()
+        CType(Me.btBackup, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'bkEstilo
+        '
+        Me.bkEstilo.BackgroundImageStyle.Alpha = 100
+        Me.bkEstilo.BackgroundImageStyle.ImageEffect = Klik.Windows.Forms.v1.Common.ImageEffect.Mirror
+        Me.bkEstilo.BorderShape.BottomLeft = Klik.Windows.Forms.v1.Common.BorderShapes.Rectangle
+        Me.bkEstilo.BorderShape.BottomRight = Klik.Windows.Forms.v1.Common.BorderShapes.Rectangle
+        Me.bkEstilo.FormOffice2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack
+        Me.bkEstilo.MainContainer = Me
+        Me.bkEstilo.ToolStripOffice2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack
+        '
+        'ElGroupBox1
+        '
+        Me.ElGroupBox1.BackgroundStyle.GradientAngle = 45.0!
+        Me.ElGroupBox1.BorderStyle.SmoothingMode = Klik.Windows.Forms.v1.Common.SmoothingModes.AntiAlias
+        Me.ElGroupBox1.CaptionStyle.BackgroundStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid
+        Me.ElGroupBox1.CaptionStyle.BackgroundStyle.SolidColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ElGroupBox1.CaptionStyle.BorderStyle.BorderShape.BottomLeft = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.ElGroupBox1.CaptionStyle.BorderStyle.BorderShape.BottomRight = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.ElGroupBox1.CaptionStyle.BorderStyle.BorderShape.TopLeft = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.ElGroupBox1.CaptionStyle.BorderStyle.BorderShape.TopRight = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.ElGroupBox1.CaptionStyle.BorderStyle.SmoothingMode = Klik.Windows.Forms.v1.Common.SmoothingModes.AntiAlias
+        Me.ElGroupBox1.CaptionStyle.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid
+        Me.ElGroupBox1.CaptionStyle.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(CType(CType(253, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(191, Byte), Integer))
+        Me.ElGroupBox1.CaptionStyle.ForegroundImageStyle.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ElGroupBox1.CaptionStyle.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack
+        Me.ElGroupBox1.CaptionStyle.Size = New System.Drawing.Size(156, 24)
+        Me.ElGroupBox1.CaptionStyle.TextStyle.ForeColor = System.Drawing.Color.White
+        Me.ElGroupBox1.CaptionStyle.TextStyle.Text = "Respaldo de Base de Datos"
+        Me.ElGroupBox1.CaptionStyle.TextStyle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.ElGroupBox1.Controls.Add(Me.btExamine)
+        Me.ElGroupBox1.Controls.Add(Me.btBackup)
+        Me.ElGroupBox1.Controls.Add(Me.txtPath)
+        Me.ElGroupBox1.Controls.Add(Me.Label2)
+        Me.ElGroupBox1.Location = New System.Drawing.Point(38, 43)
+        Me.ElGroupBox1.Name = "ElGroupBox1"
+        Me.ElGroupBox1.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ModernBlack
+        Me.ElGroupBox1.Padding = New System.Windows.Forms.Padding(4, 27, 4, 3)
+        Me.ElGroupBox1.Size = New System.Drawing.Size(328, 116)
+        Me.ElGroupBox1.TabIndex = 2
+        '
+        'btExamine
+        '
+        Me.btExamine.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton
+        Me.btExamine.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground
+        Me.btExamine.Image = Global.appFacturacion.My.Resources.Resources.examinar16x16
+        Me.btExamine.Location = New System.Drawing.Point(294, 40)
+        Me.btExamine.Name = "btExamine"
+        Me.btExamine.Size = New System.Drawing.Size(27, 23)
+        Me.btExamine.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled
+        Me.btExamine.TabIndex = 31
+        '
+        'btBackup
+        '
+        Me.btBackup.BorderStyle.BorderShape.BottomLeft = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.btBackup.BorderStyle.BorderShape.BottomRight = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.btBackup.BorderStyle.BorderShape.TopLeft = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.btBackup.BorderStyle.BorderShape.TopRight = Klik.Windows.Forms.v1.Common.BorderShapes.Circle
+        Me.btBackup.BorderStyle.EdgeRadius = 7
+        Me.btBackup.BorderStyle.SmoothingMode = Klik.Windows.Forms.v1.Common.SmoothingModes.AntiAlias
+        Me.btBackup.FlashStyle.PaintType = Klik.Windows.Forms.v1.Common.PaintTypes.Solid
+        Me.btBackup.FlashStyle.SolidColor = System.Drawing.Color.FromArgb(CType(CType(253, Byte), Integer), CType(CType(240, Byte), Integer), CType(CType(191, Byte), Integer))
+        Me.btBackup.ForegroundImageStyle.Image = Global.appFacturacion.My.Resources.Resources.Entrar
+        Me.btBackup.ForegroundImageStyle.ImageAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.btBackup.Location = New System.Drawing.Point(15, 68)
+        Me.btBackup.Name = "btBackup"
+        Me.btBackup.Office2007Scheme = Klik.Windows.Forms.v1.Common.Office2007Schemes.ClassicBlack
+        Me.btBackup.Size = New System.Drawing.Size(116, 31)
+        Me.btBackup.TabIndex = 9
+        Me.btBackup.TextStyle.Font = New System.Drawing.Font("Arial", 8.25!)
+        Me.btBackup.TextStyle.Text = "Crear Backup"
+        Me.btBackup.TextStyle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'txtPath
+        '
+        Me.txtPath.BackColor = System.Drawing.Color.White
+        '
+        '
+        '
+        Me.txtPath.Border.Class = "TextBoxBorder"
+        Me.txtPath.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square
+        Me.txtPath.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPath.Location = New System.Drawing.Point(77, 42)
+        Me.txtPath.Name = "txtPath"
+        Me.txtPath.ReadOnly = True
+        Me.txtPath.Size = New System.Drawing.Size(218, 20)
+        Me.txtPath.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(12, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(56, 14)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Directorio:"
+        '
+        'frmBackup
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(406, 202)
+        Me.Controls.Add(Me.ElGroupBox1)
+        Me.DoubleBuffered = True
+        Me.Font = New System.Drawing.Font("Arial", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "frmBackup"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Crear Respaldo"
+        CType(Me.bkEstilo, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.ElGroupBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ElGroupBox1.ResumeLayout(False)
+        Me.ElGroupBox1.PerformLayout()
+        CType(Me.btBackup, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents bkEstilo As Klik.Windows.Forms.v1.Common.KFormManager
+    Friend WithEvents ElGroupBox1 As Klik.Windows.Forms.v1.EntryLib.ELGroupBox
+    Friend WithEvents txtPath As DevComponents.DotNetBar.Controls.TextBoxX
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btBackup As Klik.Windows.Forms.v1.EntryLib.ELButton
+    Friend WithEvents btExamine As DevComponents.DotNetBar.ButtonX
+    Friend WithEvents fbdExamine As System.Windows.Forms.FolderBrowserDialog
+End Class
---
+++ App/appFacturacion/appFacturacion/Views/Security/frmBackup.vb
@@ -0,0 +1,64 @@
+﻿Public Class frmBackup
+
+    Private Sub btExamine_Click(sender As Object, e As EventArgs) Handles btExamine.Click
+
+        Me.SelectPath()
+
+    End Sub
+
+    Private Sub btBackup_Click(sender As Object, e As EventArgs) Handles btBackup.Click
+
+        Me.CreateBackup()
+
+    End Sub
+
+    Private Sub SelectPath()
+
+        If fbdExamine.ShowDialog() = Windows.Forms.DialogResult.OK Then
+
+            txtPath.Text = fbdExamine.SelectedPath
+
+        End If
+
+    End Sub
+
+    Private Function ValidatePath() As Boolean
+
+        If txtPath.Text.Trim = "" Then
+
+            Config.MsgErr("Seleccione un directorio")
+
+            Return False
+
+        Else
+
+            Return True
+
+        End If
+
+    End Function
+
+    Private Sub CreateBackup()
+
+        Try
+
+            If Me.ValidatePath() Then
+
+                Dim security As New Sadara.BusinessLayer.Security
+
+                security.CreateBackupOfDbAsync(txtPath.Text)
+
+                Config.MsgInfo("Respaldo creado correctamente.")
+
+            End If
+
+        Catch ex As Exception
+
+            Config.MsgErr(ex.Message)
+
+        End Try
+
+    End Sub
+
+
+End Class
